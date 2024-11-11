f9da75b223a64f21f1791137a6749e8ad7d38a77
Xa-C24 <9335@holbertonstudents.com>
2024-11-11 14:11:33 +0100

    For...of Loops

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+// 10-loops.js
+
+export default function appendToEachArrayValue(array, appendString) {
+    for (const [index, value] of array.entries()) {
+      array[index] = appendString + value;
+    }
+    return array;
+  }
+  
