5f17e82fdeeb3d5e52993a6425914c2b3ba6fd5b
Xa-C24 <9335@holbertonstudents.com>
2024-11-13 12:14:11 +0100

    Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,14 +1,13 @@
 // 1-block-scoped.js
 
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
-  
-    if (trueOrFalse) {
-      let task = true; // portée limitée au bloc if
-      let task2 = false; // portée limitée au bloc if
-    }
-  
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true; // portée limitée au bloc if
+    task2 = false; // portée limitée au bloc if
   }
-  
+
+  return [task, task2];
+}
