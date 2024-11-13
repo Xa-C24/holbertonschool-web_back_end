633018d05079991a849e326e8dc93367b27736f9
Xa-C24 <9335@holbertonstudents.com>
2024-11-13 16:56:52 +0100

    add file eslint.config

---
+++ ES6_basic/eslint.config.js
@@ -0,0 +1,18 @@
+// eslint.config.js
+export default [
+    {
+      ignores: ["node_modules/**"],
+    },
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "off",
+      },
+    },
+  ];
+  
