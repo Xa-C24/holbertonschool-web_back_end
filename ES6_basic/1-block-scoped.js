429e8a647681656b006dfb797cb30edfaca58a08
Xa-C24 <9335@holbertonstudents.com>
2024-11-13 15:43:50 +0100

    Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,9 @@
-export default function taskBlock() {
-  const task = false;
-  const task2 = true;
-
-  return [task, task2];
-}
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      return [task, task2];
+    }
+    return [task, task2];
+  }
