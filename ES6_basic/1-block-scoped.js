6a61c2aeb35a3ab0c5ddd3562f9bdd5279dd272c
Xa-C24 <9335@holbertonstudents.com>
2024-11-13 12:36:55 +0100

    Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,6 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock() {
   let task = false;
   let task2 = true;
-
-  if (trueOrFalse) {
-    // Déclaration de nouvelles variables pour le bloc if
-    const innerTask = true;
-    const innerTask2 = false;
-    return [innerTask, innerTask2];
-  }
 
   return [task, task2];
 }
