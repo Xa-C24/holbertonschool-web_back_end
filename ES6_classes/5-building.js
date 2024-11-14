393f280dacb2b28a6c604a6dfb2862be1e0ba8d0
Xa-C24 <9335@holbertonstudents.com>
2024-11-14 08:28:07 +0100

    Implement a class named Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,20 @@
 class Building {
-    constructor(sqft) {
-        if (typeof sqft !== 'number') {
-            throw new TypeError('sqft must be a number');
-        }
-        this._sqft = sqft;
-
-        // Vérifie si la méthode `evacuationWarningMessage` est implémentée
-        if (this.evacuationWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
+    this._sqft = sqft;
 
-    // Getter pour `sqft`
-    get sqft() {
-        return this._sqft;
+    // Vérifie si la méthode `evacuationWarningMessage` est implémentée
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+  }
+
+  // Getter pour `sqft`
+  get sqft() {
+    return this._sqft;
+  }
 }
 
 export default Building;
