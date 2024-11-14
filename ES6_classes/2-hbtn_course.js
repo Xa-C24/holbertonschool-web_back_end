ab5f03995fecdac33d37546fe6f0fc34b9437e27
Xa-C24 <9335@holbertonstudents.com>
2024-11-14 08:29:44 +0100

    Implement a class named HolbertonCourse

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,42 +1,49 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    // Validation des types dans le constructeur
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
 
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  // Getter et Setter pour name
+  // Getter pour name
   get name() {
     return this._name;
   }
+
+  // Setter pour name avec vérification de type
   set name(value) {
-    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
 
-  // Getter et Setter pour length
+  // Getter pour length
   get length() {
     return this._length;
   }
+
+  // Setter pour length avec vérification de type
   set length(value) {
-    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = value;
   }
 
-  // Getter et Setter pour students
+  // Getter pour students
   get students() {
     return this._students;
   }
+
+  // Setter pour students avec vérification de type
   set students(value) {
-    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
     }
     this._students = value;
   }
