5d1824c40a79b4bbc78022528cf543457d7f126b
Xa-C24 <9335@holbertonstudents.com>
2024-11-17 10:21:44 +0100

    modif fix eslint

--- ES6_basic/.eslintrc.cjs
+++ ES6_basic/.eslintrc.cjs
@@ -1,35 +1,27 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  plugins: [
+    'jest',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
+    'jest/require-hook': 'warn',
+    'max-len': ['error', { code: 120 }],
+  },
+};
