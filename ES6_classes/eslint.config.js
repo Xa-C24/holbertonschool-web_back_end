521da455ffbfd3ed1b35c409ffd1117221159689
Xa-C24 <9335@holbertonstudents.com>
2024-11-17 11:07:59 +0100

    fix fucking error

---
+++ ES6_classes/eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        ignores: ['node_modules/**'],
+    },
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            // Ajoute ici les règles spécifiques si besoin
+        },
+    },
+];
