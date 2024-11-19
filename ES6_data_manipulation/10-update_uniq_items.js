185e4789b12bbf6126d65c0004ae2d4df3c64031
Xa-C24 <9335@holbertonstudents.com>
2024-11-19 09:26:55 +0100

    More map data structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,11 +2,13 @@ export default function updateUniqueItems(items) {
   if (!(items instanceof Map)) {
     throw new Error("Cannot process");
   }
+
   // Parcourir la Map et mettre à jour les quantités
   items.forEach((value, key) => {
     if (value === 1) {
       items.set(key, 100);
     }
-  })
-  return updatedItems;
+  });
+
+  return items; // Retour explicite de la Map (optionnel)
 }
