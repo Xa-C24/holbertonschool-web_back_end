c6e2ea9ea9192ab9d25f6ef2d669520e2e0d49e4
Xa-C24 <9335@holbertonstudents.com>
2024-11-20 15:33:16 +0100

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,26 +1,12 @@
 // Importation des fonctions nécessaires depuis leurs fichiers respectifs.
+import { resolvePlugin } from '@babel/core';
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 // Fonction principale pour gérer l'inscription de l'utilisateur et l'upload de sa photo.
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Appel des fonctions pour retourner des Promises
-  const signUpUserPromise = signUpUser(firstName, lastName); // Promise pour l'inscription
-  const uploadPhotoPromise = uploadPhoto(fileName); // Promise pour l'upload de la photo
-
-  // Utilisation de Promise.allSettled pour attendre que toutes les Promises soient "résolues" (fulfilled ou rejected)
-  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
-    .then((results) => {
-      // Transformation des résultats en un tableau structuré avec status et value/reason
-      return results.map((result) => {
-        if (result.status === 'fulfilled') {
-          // Si la Promise est résolue avec succès
-          return { status: result.status, value: result.value };
-        }
-        // Si la Promise est rejetée, inclure la raison
-        return { status: result.status, value: result.reason };
-      });
-    });
+  return Promise.allSettled (
+    [signUpUser(firstName, lastName), uploadPhoto(fileName)]
+  )
 }
-
 // return pending = en cours d'execution en attente
