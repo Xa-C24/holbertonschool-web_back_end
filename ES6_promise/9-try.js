b1aaa3e199a4d04903cc02e4cb0b4c068ed7e46f
Xa-C24 <9335@holbertonstudents.com>
2024-11-20 15:12:38 +0100

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+export default function guardrail(mathFunction) {
+  const queue = []; // Initialisation le tableau queue
+
+  try {
+     // Essayer d'exécuter mathFunction et ajouter son résultat au tableau
+      const result = mathFunction();
+      queue.push(result);
+      // Si une erreur est levée, ajouter le message de l'erreur au tableau
+  } catch (error) {
+    queue.push(error.message);
+  }
+
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
