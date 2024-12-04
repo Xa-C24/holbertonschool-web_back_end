9684183eb1a3b0fae0cd6a5b52e21bfd16fe2566
Xa-C24 <9335@holbertonstudents.com>
2024-12-04 09:25:16 +0100

    add docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""a script for type-annotated function add that
+takes a float a and a float b as arguments
+and returns their sum as a float."""
 
 def add(a: float, b: float) -> float:
     """
