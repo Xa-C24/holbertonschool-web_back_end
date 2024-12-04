4d0769239d7080758eb8cce1290939bafd9be7e7
Xa-C24 <9335@holbertonstudents.com>
2024-12-04 09:38:58 +0100

    add docstring plus pycodstyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""A module for converting a float to its string representation."""
+
+
 def to_str(n: float) -> str:
-    """Returns the string representation of the float."""
+    """
+    Convert a floating-point number to its string representation.
+
+    Args:
+        n (float): The floating-point number to convert.
+
+    Returns:
+        str: The string representation of the number.
+    """
+
     return str(n)
