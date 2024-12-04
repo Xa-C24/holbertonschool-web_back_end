87329a700efc4cbdcba4742fca18c2c386f52857
Xa-C24 <9335@holbertonstudents.com>
2024-12-04 09:52:13 +0100

    add docstring plus pycodstyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""A module for creating a multiplier function."""
+
+
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Return a function that multiplies a float"""
+    """
+    Create a function that multiplies a float by a given multiplier.
+
+    Args:
+        multiplier (float): The multiplier to use.
+
+    Returns:
+        Callable[[float], float]: A function that takes a float and returns
+        the product of the float and the multiplier.
+    """
     def multiplier_function(value: float) -> float:
+        """
+        Multiply a given float by the multiplier.
+
+        Args:
+            value (float): The float to be multiplied.
+
+        Returns:
+            float: The result of the multiplication.
+        """
         return value * multiplier
     return multiplier_function
