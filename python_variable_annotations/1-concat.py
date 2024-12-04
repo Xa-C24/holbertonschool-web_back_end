08a3626f37687905932fc86945a6f52822e05bb3
Xa-C24 <9335@holbertonstudents.com>
2024-12-04 09:31:56 +0100

    add docstind plus pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Concat two string and returns result ."""
+
+
 def concat(str1: str, str2: str) -> str:
-    """Concat two string and returns result ."""
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated result of str1 and str2.
+    """
+
     return str1 + str2
