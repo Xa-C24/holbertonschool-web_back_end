bb45bf6f47190773f76b60b7594dfeb9a95d70e8
Xa-C24 <9335@holbertonstudents.com>
2024-12-09 10:49:59 +0100

    Creat generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 """Creates a generator"""
-
 import asyncio
 import random
 
 
 async def async_generator():
-    """
-      Coroutine that generates 10 random floating-point numbers asynchronously.
-
-      At each iteration:
-      - Waits asynchronously for 1 second.
-      - Yields a random floating-point number between 0 and 10.
-
-      Returns:
-          An asynchronous generator yielding 10 random numbers (float).
-      """
-    for _ in range(10):  # Boucle 10fois
-      await asyncio.sleep(1)  # Attend 1seconde
-      yield random.uniform(0, 10)  # Génére un nombre aléatoire
+    """Each time asynchronously waits 1 second,
+        then yield a random number between 0 and 10"""
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
