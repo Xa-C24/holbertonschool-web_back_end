3b8f8274715e65f2876cebb8893424ad2b5bc5eb
Xa-C24 <9335@holbertonstudents.com>
2024-12-11 09:57:59 +0100

    Python function that inserts a new document in a collection

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ function that inserts a new document in a collection """
 
+
 def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document in a collection based on kwargs.
