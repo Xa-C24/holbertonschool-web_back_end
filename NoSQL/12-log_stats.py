de243a58e4ef97cf6815278e76b199ce7521b4b0
Xa-C24 <9335@holbertonstudents.com>
2024-12-12 11:11:11 +0100

    test mango

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+"""
+Python script that provides some stats
+about Nginx logs stored in MongoDB.
+"""
+
 from pymongo import MongoClient
 
 
 def log_stats():
     """
-    Affiche des statistiques sur les logs Nginx dans la base de données MongoDB.
+    Affiche des statistiques sur les logs Nginx
+    dans la base de données MongoDB.
     """
     # Connexion à MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017')
@@ -24,7 +30,8 @@ def log_stats():
 
     # Nombre de requêtes GET avec path /status
     status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"})
+        {"method": "GET", "path": "/status"}
+    )
     print(f"{status_check} status check")
 
 
