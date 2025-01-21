b1a0aa18463909c3733ec6003bb4655bf32a9b95
Xa-C24 <9335@holbertonstudents.com>
2025-01-21 09:27:29 +0000

    Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// Affiche un message d'accueil
+console.log('Welcome to Holberton School, what is your name?');
+
+// Écouter les données saisies par l'utilisateur
+process.stdin.on('data', (input) => {
+    // Convertir l'entrée en chaîne de caractères et supprimer les espaces inutiles
+    const name = input.toString().trim();
+    console.log(`Your name is: ${name}`);
+});
+
+// Écouter la fin du flux d'entrée (CTRL+D ou pipe)
+process.stdin.on('end', () => {
+    console.log('This important software is now closing')
+});
