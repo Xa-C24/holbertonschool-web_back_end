1d1adcf7487d5537c2f6b1f6b30ecdfed05a0456
Xa-C24 <9335@holbertonstudents.com>
2025-01-21 14:55:29 +0000

    Server Http

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// Créer un serveur HTTP
+const app = http.createServer((req, res) => {
+    // Configurer les en têtes HTTP
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+
+    // Envoyer la réponse
+    res.end('Hello Holberton School');
+});
+
+// Faire écouter le serveur sur le port 1245
+app.listen(1245);
+
+// Exporter le serveur
+module.exports = app;
