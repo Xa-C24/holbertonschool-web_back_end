a97fd05ae2c720799a4c1312dc274f371163a4f3
Xa-C24 <9335@holbertonstudents.com>
2025-01-22 10:57:30 +0000

    Create a small HTTP server using Express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,18 @@
+// Importation de Express
+const express = require("express");
+
+// Création de l'application Express
+const app = express();
+
+// Route pour l'endpoint /
+app.get("/", (req, res) => {
+  res.send("Hello Holberton School!");
+});
+
+// Configuration pour écouter sur le port 1245
+app.listen(1245, () => {
+  console.log("Server is running on http://localhost:1245");
+});
+
+// Exportation de l'application Express
+module.exports = app;
