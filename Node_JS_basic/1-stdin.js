a6c31fb1273dfa3078e3a1dde061d8cea322f617
Xa-C24 <9335@holbertonstudents.com>
2025-01-22 10:33:32 +0000

    fix task 1 and change data / readable

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,18 @@
 // Affiche un message d'accueil
-console.log('Welcome to Holberton School, what is your name?');
+console.log("Welcome to Holberton School, what is your name?");
 
 // Écouter les données saisies par l'utilisateur
-process.stdin.on('data', (input) => {
+process.stdin.on("readable", () => {
+  // Lire les données saisies
+  const input = process.stdin.read();
+  if (input !== null) {
     // Convertir l'entrée en chaîne de caractères et supprimer les espaces inutiles
     const name = input.toString().trim();
     console.log(`Your name is: ${name}`);
+  }
 });
 
 // Écouter la fin du flux d'entrée (CTRL+D ou pipe)
-process.stdin.on('end', () => {
-    console.log('This important software is now closing')
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
 });
