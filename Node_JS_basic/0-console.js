592e39612bd51cde7004a060fa6952e41ce65364
Xa-C24 <9335@holbertonstudents.com>
2025-01-22 09:57:44 +0000

    test for fix task0

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
-const displayMessage = (message) => {
-    console.log(message);
-  };
-  
-  module.exports = displayMessage;
-  
+// Executing basic javascript with Node JS
+function displayMessage(message) {
+  console.log(message);
+};
+
+module.exports = displayMessage;
