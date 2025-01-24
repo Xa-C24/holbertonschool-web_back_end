dcf649707a11156e3f5dfc3347ea1af763c008cc
Xa-C24 <9335@holbertonstudents.com>
2025-01-24 08:49:11 +0100

    fix task change input  -> name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
-// Affiche un message d'accueil
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Écouter les données saisies par l'utilisateur
-process.stdin.on("readable", () => {
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-// Écouter la fin du flux d'entrée (CTRL+D ou pipe)
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
