aa2ddf13b77de84d3d4c965f4bf379e2eb876a63
Xa-C24 <9335@holbertonstudents.com>
2025-01-24 08:48:14 +0100

    fix task change input  -> name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,6 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.on("readable", () => {
   const name = process.stdin.read();
   if (name !== null) {
-    const name = input.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
   }
 });
